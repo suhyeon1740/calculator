6cd84fb433ee285cdff67b038282ee3dd54629fc
Elly (박수현) <elly@treenod.com>
2019-11-20 10:32:55 +0900

    eval 예외처리

--- script.js
+++ script.js
@@ -1,37 +1,35 @@
-var obj = {        
-    resultClickFlag: false,
-    input: document.querySelector('input'),
-    addText: (e) => {
-        if (obj.resultClickFlag) {
-            obj.input.value = ""
-            obj.resultClickFlag = false
-        }
-        obj.input.value += e.target.innerText
-    },    
-    resultClick: () => {
-        console.log(eval(obj.input.value))
-        obj.input.value = eval(obj.input.value)
-        obj.resultClickFlag = true
-    },
-    acClick: () => {
-        obj.input.value = ""        
-    }
-}
-// 숫자 클릭            
-let numbers = document.getElementsByClassName('number')
-for (let i = 0; i < numbers.length; i++) {
-    numbers[i].addEventListener('click', obj.addText)
-}
+let resultClickFlag = false
+let calculateClickFlag = false
+const input = document.querySelector('input')
 
-//+ 클릭하면 연산 변수에 저장
-let calculate = document.getElementsByClassName('calculate');
-for (let i = 0; i < calculate.length; i++) {    
-    calculate[i].addEventListener('click',obj.addText)
+// 버튼 클릭            
+let numbers = document.getElementsByClassName('btn')
+for (let i = 0; i < numbers.length; i++) {
+    numbers[i].addEventListener('click', (e) => {
+        // = 클릭하면 초기화 후  연산
+        if (resultClickFlag) {
+            input.value = ""
+            resultClickFlag = false
+        }        
+        if (!isNaN(e.target.innerText)) calculateClickFlag = false
+        else {
+            // 연산자 중복클릭 X
+            if (calculateClickFlag) return;
+            calculateClickFlag = true
+        }
+        input.value += e.target.innerText
+    })
 }
 
 //ac 클릭하면 초기화
 let ac = document.getElementById("ac")
-ac.addEventListener('click', obj.acClick)
+ac.addEventListener('click', () => {
+    input.value = ""
+})
 
 // = 클릭하면 결과 표출
-document.getElementById('result').addEventListener('click', obj.resultClick)
+document.getElementById('result').addEventListener('click', () => {    
+    if (!input.value || calculateClickFlag) return
+    input.value = eval(input.value)
+    resultClickFlag = true
+})
